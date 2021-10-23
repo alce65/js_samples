9fdd64e2b8999d0e2548c513e94db08208ea86a1
Alejandro Cerezo <alce65@hotmail.es>
2021-10-23 13:07:46 +0200

    Add ES Modules

---
+++ sample-modules/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import { suma } from './lib.js';
+
+console.log('SUMA: ', suma(3, 5));
---
+++ sample-modules/main.mjs
@@ -0,0 +1,5 @@
+// 'use strict';
+
+import { suma } from './lib.mjs';
+
+console.log('SUMA: ', suma(3, 5));
